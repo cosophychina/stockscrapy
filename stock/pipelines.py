1e1d8430068009618d7fd8d6b299516a7958c493
cosophychina <wyrja@126.com>
2018-06-04 00:22:32 +0800

    modify pipelines and spider

--- stock/pipelines.py
+++ stock/pipelines.py
@@ -9,3 +9,49 @@
 class StockPipeline(object):
     def process_item(self, item, spider):
         return item
+
+class StockMysqlPipelime(MysqlPipeline):
+	"""docstring for StockMysqlPipelime"""
+	def process_item(self, item, spider):
+		sql = ""
+		super().process_item(item, spider, [sql])
+		pass
+		
+
+class MysqlPipeline(object):
+    ''' MySQL数据处理类 '''
+    def __init__(self,host,database,user,password,port):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.port = port
+        self.db=None
+        self.cursor=None
+
+    @classmethod
+    def from_crawler(cls,crawler):
+        return cls(
+            host = crawler.settings.get("MYSQL_HOST"),
+            database = crawler.settings.get("MYSQL_DATABASE"),
+            user = crawler.settings.get("MYSQL_USER"),
+            password = crawler.settings.get("MYSQL_PASS"),
+            port = crawler.settings.get("MYSQL_PORT")
+        )
+
+    def open_spider(self,spider):
+        self.db = pymysql.connect(self.host,self.user,self.password,self.database,charset='utf8',port=self.port)
+        self.cursor = self.db.cursor()
+
+    def process_item(self, item, spider, sqlList = []):
+        try:
+	        for sql in sqlList:
+	        	self.cursor.execute(sql)
+	        self.db.commit()
+        except:
+        	self.db.rollback()
+        
+        return item
+
+    def close_spider(self,spider):
+        self.db.close()
