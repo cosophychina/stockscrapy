ef8f40c21cdc8c65cdfa1f434234fbf20842e807
cosophychina <wyrja@126.com>
2018-06-11 18:22:13 +0800

    add useragent middleware

--- stock/settings.py
+++ stock/settings.py
@@ -35,7 +35,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -60,9 +60,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'stock.middlewares.StockDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'stock.middlewares.UserAgent': 543
+}
+#'stock.middlewares.StockDownloaderMiddleware': 543,
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
